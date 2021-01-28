e4dd1fbcd0d60004919e85359115ed07c42c1362
laurent <laurent.duchaussoy59@gmail.com>
2021-01-28 23:20:25 +0100

    feat(ButtonClipboard): add button for copy code

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const buttonClipBoardTemplate = `<div class='button-copy-clipboard' onclick="copyToClipBoard(this.parentElement.textContent)"><button>copy</button></div>`
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.getElementsByClassName('language-ruby');
+    for(let element of elements){
+        element.innerHTML += element.innerHTML + buttonClipBoardTemplate;
+    }
+});
+
+copyToClipBoard = (e) => {
+    navigator.clipboard.writeText(e)
+};
