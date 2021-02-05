0599a981799c15cf2a90c101717681e2455067bc
laurent <laurent.duchaussoy59@gmail.com>
2021-02-05 23:03:22 +0100

    feat(footer): add new block and refacto design
    - add logo
    - manage design for articles
    - refacto color select 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const buttonClipBoardTemplate = `<button class='button-copy-clipboard btn btn-light btn-sm' onclick="copyToClipBoard(this.parentElement.textContent)">copy</button>`
+const buttonClipBoardTemplate = `<button class='button-copy-clipboard btn btn-light btn-sm' onclick="copyToClipBoard(this.parentElement.textContent)">Copier</button>`
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -8,7 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     // init select2
     $('.search-article').select2({
-        placeholder: "Search an article...",
+        placeholder: "<div style='display: flex; align-items:center;'><span class='material-icons' style='font-size:18px; margin-right: 5px'>search</span>Chercher un article ...</div>",
+        escapeMarkup: function (markup) { return markup; },
         width: '100%'
     }).on('select2:select', function (e) {
         window.location.href = e.params.data.id;
@@ -18,3 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 copyToClipBoard = (e) => {
     navigator.clipboard.writeText(e);
 };
+
+function goBackHistory() {
+    window.history.back();
+}
